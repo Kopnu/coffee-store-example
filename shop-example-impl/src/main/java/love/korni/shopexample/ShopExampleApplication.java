59edcfd0fc7018a56e6dad6ceb891da452b99ef0
Kopnu <Dzivv0@yandex.ru>
2022-04-10 12:18:14 +0400

    Initial commit

---
+++ shop-example-impl/src/main/java/love/korni/shopexample/ShopExampleApplication.java
@@ -0,0 +1,13 @@
+package love.korni.shopexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopExampleApplication.class, args);
+    }
+
+}
---
+++ shop-example-impl/src/test/java/love/korni/shopexample/ShopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package love.korni.shopexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
